971b583c434f24c551eb43e7eab64572b182510b
Alexander Pillai <pillai.alexander@gmail.com>
2020-06-24 18:33:25 -0400

    ShinyApp v1.1: professional-ized UI, fixed point/brush datatable, begun updating text for about page and plots.

--- testApp/text.R
+++ testApp/text.R
@@ -1,1 +1,5 @@
 aboutText <- "Insert text here."
+
+plotDisclaimer <- "See \"Plot Information\" below or \"About\" page for more information on how case numbers are reported and anticipated."
+
+plotClickInstr <- "Click on a column or drag over a region in the plot to see more information for the selected days."
