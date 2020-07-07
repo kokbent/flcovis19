03cb973ffd6557e28d604f39592b05a3697bfa6d
Kok Ben Toh <kokbent@ufl.edu>
2020-07-06 22:48:55 -0400

    minor fix in run.R, attempt #4

--- run.R
+++ run.R
@@ -4,7 +4,7 @@ library(shiny)
 port <- Sys.getenv('PORT')
 
 shiny::runApp(
-  appDir = paste0(getwd(), "testApp/"),
+  appDir = paste0(getwd(), "/testApp/"),
   host = '0.0.0.0',
   port = as.numeric(port)
 )
